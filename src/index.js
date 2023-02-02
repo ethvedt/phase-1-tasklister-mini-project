39078cd7163589fb88565ba33e1d8554b01868fc
Eric Thvedt <ericsthvedt@gmail.com>
2023-02-02 09:17:06 -0800

    able to submit, doesn't add anything if input form is empty

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", handleSubmit);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const li = document.createElement("li");
+    const list = document.querySelector("#tasks");
+    li.textContent = form.querySelector('[type=text]').value;
+    list.append(li);
+    if (li.textContent === "") {
+      li.remove();
+    };
+  }
 });
+
